71f42c09c1b40b38e9edd9f9f03d369bd10b684a
Faralas <alvarodavidpp@gmail.com>
2023-02-19 18:04:36 -0500

    add provider and variables

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,5 +1,24 @@
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
 module "resource_group" {
   source = "./resource_group"
+}
+
+module "storage_account" {
+  source = "./azure_storage_account"
 }
 
 module "container_registry" {
