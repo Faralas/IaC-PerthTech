fb0f65892f8e2d786eff4dccced7bd17ee53ab5a
Alvaro David Polanco <Alvaro.Polanco_EPAM@kantar.com>
2023-03-20 20:09:11 -0500

    adding depends on step

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -27,16 +27,25 @@ module "storage_account" {
   source                  = "./azure_storage_account"
   resource_group_name     = var.resource_group_name
   resource_group_location = var.resource_group_location
+  depends_on = [
+    azurerm_resource_group.resource_group
+  ]
 }
 
 module "container_registry" {
   source                  = "./container_registry"
   resource_group_name     = var.resource_group_name
   resource_group_location = var.resource_group_location
+  depends_on = [
+    azurerm_resource_group.resource_group
+  ]
 }
 
 module "kubernetes_cluster" {
   source                  = "./kubernetes_cluster_module"
   resource_group_name     = var.resource_group_name
   resource_group_location = var.resource_group_location
+  depends_on = [
+    azurerm_resource_group.resource_group
+  ]
 }
